e1f264917ce33e211e082c8b8328f3e31c777f48
kellyzc <kellyzc@rose-hulman.edu>
2018-11-26 09:54:02 -0500

    Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zach Kelly.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,28 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+window.tracer(10)
+
+turtle1 = rg.SimpleTurtle()
+turtle2 = rg.SimpleTurtle()
+turtle1.pen = rg.Pen("red", 2)
+turtle2.pen = rg.Pen("gray", 2)
+
+for i in range(300):
+    turtle1.forward(int(i / 10))
+    turtle1.left(10)
+
+for a in range(10):
+    for b in range(15):
+        turtle2.draw_circle(10)
+        turtle2.forward(20)
+        turtle2.left(b * 10)
+        turtle2.forward(20)
+    turtle2.go_to(rg.Point(0, 0))
+    turtle2.set_heading(45 * a)
+
+
+window.close_on_mouse_click()
